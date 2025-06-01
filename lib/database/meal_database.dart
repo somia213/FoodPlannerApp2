20182aa78367ae28c55ca1c79fa2d707951b9b56
SomiaAmer <105312434+somia213@users.noreply.github.com>
2025-06-01 11:52:17 +0300

    create database

---
+++ lib/database/meal_database.dart
@@ -0,0 +1,49 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:first_app/models/food_item.dart';
+
+class MealDatabase {
+// One shared instance of MealDatabase is used across the entire app.
+// A private constructor stops other classes from creating new objects.
+// Use .instance to open, update, or delete — no need to create again.
+
+  static final MealDatabase instance = MealDatabase._init();
+// instance is like the controller or manager — it gives you access to the database and controls when to open or close it.
+// _database is the actual database connection that handles the real work like inserting, updating, deleting, and querying data.
+
+  static Database? _database;
+  MealDatabase._init();
+
+//  file_path -> database file name
+  Future<Database> _initDB(String file_path) async {
+    final db_path = await getDatabasesPath();
+    final path = join(db_path, file_path);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('meals.db');
+    return _database!;
+  }
+
+  Future _createDB(Database db , int version ) async {
+    await db.execute(''' 
+     CREATE TABLE favorites (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        category TEXT,
+        area TEXT,
+        instructions TEXT,
+        thumbnail TEXT,
+        youtubeUrl TEXT,
+        ingredients TEXT
+      )
+     ''');
+  }
+}
