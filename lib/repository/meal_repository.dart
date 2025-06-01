82e4a00500d6a4f5ba6046fb7a1c4efd9edbecf2
SomiaAmer <105312434+somia213@users.noreply.github.com>
2025-06-01 18:22:57 +0300

    meal repository

---
+++ lib/repository/meal_repository.dart
@@ -0,0 +1,15 @@
+import '../database/meal_database.dart';
+import '../models/food_item.dart';
+
+// middle level between database & viewmodel 
+// Prevents the ViewModel from accessing the database directly and knowing how to write queries.
+
+class MealRepository{
+  final _db = MealDatabase.instance;
+
+  Future<void> addToFavorites(FoodItem meal) => _db.insertMeal(meal);
+  Future<void> removeFromFavorites(String id) => _db.removeMeal(id);
+  Future<List<FoodItem>> getFavorites() => _db.getAllFavorites();
+  Future<bool> isFavorite(String id) => _db.isFavorite(id);
+
+}
